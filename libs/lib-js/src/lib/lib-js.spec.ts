30ee6ae373aecf02027c30f9008a7cd50c08426e
Milena Fluck <milena.fluck@adesso.de>
2024-10-09 22:03:40 +0200

    init

---
+++ libs/lib-js/src/lib/lib-js.spec.ts
@@ -0,0 +1,7 @@
+import { libJs } from './lib-js';
+
+describe('libJs', () => {
+  it('should work', () => {
+    expect(libJs()).toEqual('lib-js');
+  });
+});
---
+++ libs/lib-js/src/lib/lib-js.ts
@@ -0,0 +1,3 @@
+export function libJs(): string {
+  return 'lib-js';
+}
