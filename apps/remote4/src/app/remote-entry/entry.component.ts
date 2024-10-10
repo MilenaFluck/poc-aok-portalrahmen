e5deb824c38d7881dbfd449a3de58372f079d6fa
Milena Fluck <milena.fluck@adesso.de>
2024-10-10 20:32:52 +0200

    combine fachanwendungen

---
+++ apps/remote4/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UiComponent } from '@poc-aok-portalrahmen/ui';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, UiComponent],
+  selector: 'app-remote4-entry',
+  templateUrl: './entry.component.html',
+  styleUrl: './entry.component.scss',
+})
+export class RemoteEntryComponent {}
